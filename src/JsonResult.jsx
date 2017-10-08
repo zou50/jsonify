5cecf67dc8c4c0cadbdaf028104dc3fd3af10b20
Danny Zou <zou50@purdue.edu>
2017-10-08 14:56:36 -0400

    Add ability to download JSON file

--- src/JsonResult.jsx
+++ src/JsonResult.jsx
@@ -3,6 +3,14 @@ import React, { Component } from 'react';
 const tabSpace = "\xa0\xa0";
 
 export default class JsonResult extends Component {
+  downloadFile = () => {
+    let elem = document.createElement("a");
+    let file = new Blob([document.getElementById('jsonResult').innerText], {type:'text/plain'});
+    elem.href = URL.createObjectURL(file);
+    elem.download = "result.json";
+    elem.click();
+  }
+
   renderResult() {
     let buffer = [];
     for (var i = 0; i < this.props.objs.length; i++) {
@@ -19,11 +27,13 @@ export default class JsonResult extends Component {
   }
 
   render() {
+    let result = this.renderResult();
     return (
       <div className="jsonResult">
+        <button onClick={this.downloadFile.bind(this)}>Download</button>
         <code>JSON Result</code>
         <div className="textArea">
-          <code id="jsonResult">&#123;<br />{this.renderResult()}<br />&#125;</code>
+          <code id="jsonResult">{"{"}<br/>{this.renderResult()}<br/>{"}"}</code>
         </div>
       </div>
     );
